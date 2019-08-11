3cf95d1cd03f345f25f67a5c6230668444ed3379
Arina Victor <arinavictor@yahoo.com>
2019-08-11 03:46:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [hoop, necklace, earring, bracelet]
+  ["ring", "necklace", "earrings", "bracelet"]
 end
 
 def add_element_to_end_of_array(array, element)
